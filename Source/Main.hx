884c08c4d5ba0f24d2b52338d852fe26b073640a
Cedrik Dubois <cedascsharp@gmail.com>
2015-05-15 23:54:23 -0400

    Removing empty lines.

--- Source/Main.hx
+++ Source/Main.hx
@@ -7,22 +7,17 @@ class Main extends Sprite {
 
 	public function new () {
 		super ();
-
 		trace( "Initializing RoomManager..." );
 		RoomManager.Init( this );
-
 		trace( "There is a total of "+RoomManager.GetNumber()+" rooms." );
 		RoomManager.Add( "Room", "rooms.Room" );
-
 		trace( "There is a total of "+RoomManager.GetNumber()+" rooms." );
 		trace( "Going to room 'Room'..." );
 		RoomManager.GotoRoom( "Room" );
-
 		RoomManager.Add( "Room2", "rooms.Room" );
 		trace( "There is a total of "+RoomManager.GetNumber()+" rooms." );
 		trace( "Going to room 'Room2'..." );
 		RoomManager.GotoRoom( "Room2" );
-
 		trace( "Destroying Room Manager..." );
 		RoomManager.Destroy();
 		trace( "There is a total of "+RoomManager.GetNumber()+" rooms." );
